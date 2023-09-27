2eb65fc84903f66de7bd1c7dc6cbf6244ce5c3af
Zoe Rodriguez <zoe.rod9767@gmail.com>
2023-09-27 18:14:04 -0500

    create base structure

--- main.c
+++ main.c
@@ -1,22 +1,122 @@
 // Zoe Rodriguez
-#include  <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
 
-int main (){
-    puts("hello from main");
-    return 0;
+struct ThreadIndexes
+{
+    int first;
+    int last;
+};
+
+void *mergeArray(void *input)
+{
+
+    sleep(1);
+    printf("first is %d, %d \n", ((struct ThreadIndexes *)input)->first, ((struct ThreadIndexes *)input)->last);
+
+    // should merge two arrays into one
+
+    return NULL;
 }
 
-// randomly generates an array of double values with a given size (say, N=1000), 
-// and divides it into two smaller arrays of equal size. 
+void *sortingAverage(void *vargp)
+{
+    sleep(1);
+    printf("sorting average \n");
 
+    /* implement selection or insertion sort O(n^2) */
+    /* return sorted array and overallAvg */
+    return NULL;
+}
 
-// It then passes eachm sub-array to sorting_avg thread (creates two copies of 
-// the same thread)
+double randomNumber()
+{
+    double range = (1000 - 1);
+    double div = RAND_MAX / range;
+    return 1 + (rand() / div);
+}
 
+int main(int argCount, char *arvg[])
+{
+    puts("hello from main");
 
-// and waits until these two sub-arrays are sorted and the average of the values 
-// in each array are computed. 
+    // get desired length from user from argv
+    int desiredLength = 10;
 
+    // create array A ( n double values) and randomly generate these values
+    // also create arrays B and C with the same size of A
+    //                    Afirsthalf and AsecondHalf with the half size of A
 
-// It then calls merging_avg thread by passing the already sorted two sub-arrays, 
-// and the averages of the first and second sub-arrays as parameters.
+    double a[desiredLength];
+    double b[desiredLength];
+    double c[desiredLength];
+
+    double aFirstHalf[desiredLength / 2];
+    double aSecondHalf[desiredLength / 2];
+
+    // for each item in array,
+    for (int i = 0; i <= desiredLength - 1; i++)
+    {
+        // insert random number between 1 and 1000 into arrayToSort's index
+        a[i] = randomNumber();
+    }
+
+    // copy a into b
+    for (int j = 0; j <= desiredLength - 1; j++)
+    {
+        b[j] = a[j];
+    }
+    //-------------------- ONE THREAD CASE --------------------
+    // create thB sortThread_avg to sort B and compute its average
+
+    pthread_t sortingAvg;
+    printf("Before Thread\n");
+    struct ThreadIndexes *input = malloc(sizeof(struct ThreadIndexes));
+    input->first = 0;
+    input->last = 9;
+    pthread_create(&sortingAvg, NULL, sortingAverage, input); // firgure out how to pass array
+
+    pthread_join(sortingAvg, NULL);
+    printf("After Thread\n");
+
+    //-------------------- TWO THREADS CASE --------------------
+    // copy A into Afirsthalf  and AsecondHalf
+    for (int j = 0; j <= (desiredLength / 2) - 1; j++)
+    {
+        aFirstHalf[j] = a[j];
+    }
+    for (int j = (desiredLength / 2); j <= desiredLength - 1; j++)
+    {
+        aSecondHalf[j] = a[j];
+    }
+
+    pthread_t sortingAverageFirstHalf;
+    printf("Before Thread\n");
+    struct ThreadIndexes *firstInput = malloc(sizeof(struct ThreadIndexes));
+    firstInput->first = 0;
+    firstInput->last = 4;
+    pthread_create(&sortingAverageFirstHalf, NULL, sortingAverage, firstInput);
+
+    pthread_t sortingAverageSecondHalf;
+    printf("Before Thread\n");
+    struct ThreadIndexes *secondInput = malloc(sizeof(struct ThreadIndexes));
+    secondInput->first = 5;
+    secondInput->last = 9;
+    pthread_create(&sortingAverageSecondHalf, NULL, sortingAverage, secondInput);
+
+    pthread_join(sortingAverageFirstHalf, NULL);
+    pthread_join(sortingAverageSecondHalf, NULL);
+    printf("After Thread\n");
+
+    pthread_t mergingThread;
+    printf("Before Thread\n");
+    // we don't need to pass this object to this thread, because its job is different
+    struct ThreadIndexes *mergingInput = malloc(sizeof(struct ThreadIndexes));
+    mergingInput->first = 5;
+    mergingInput->last = 9;
+    pthread_create(&mergingThread, NULL, mergeArray, mergingInput);
+
+    return 0;
+}
